ffb2631c87f466701ff16d2a9524e38e4922bd33
Odalys z <Odalys z@ODALYSZ-PC>
2023-06-06 00:58:41 -0600

    Agregar archivos de proyecto.

---
+++ Tabla/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
